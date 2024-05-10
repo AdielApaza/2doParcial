128222de8da2985a2441b2c1101b7bac089d7962
Adiel Apaza <liymasteralucard@gmail.com>
2024-05-10 09:38:39 -0400

    composicion de componentes

--- ExamParcial2-main/src/components/Card.jsx
+++ ExamParcial2-main/src/components/Card.jsx
@@ -5,8 +5,23 @@ import { NavLink } from "react-router-dom";
 const Card = () => {
   return (
     <NavLink className="nav-link">
+      <div className="superior">
       <div className="componente1">
-        
+        <img src="../assets/8bed0ee79117e6d26de911a4ac519346.jfif" alt="" />
+      </div>
+      <div className="componente2">
+        <p>
+          <h5>Terapia 2</h5>
+        </p>
+        <p>
+          <h5>Ejercicios Pelvicos</h5>
+        </p>
+      </div>
+      <div className="componente3">
+        <button>
+          <h1>3</h1>
+        </button>
+      </div>
       </div>
     </NavLink>
   );
