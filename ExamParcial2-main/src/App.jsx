2a0485eccda5bb1a7e400fdaef5aac7e214ed92b
Adiel Apaza <liymasteralucard@gmail.com>
2024-05-10 09:17:16 -0400

    primer componente

---
+++ ExamParcial2-main/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./routes/Navigation";
+import "./App.css";
+
+
+function App() {
+  
+  return (
+    <>
+      <Navigation/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ ExamParcial2-main/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+import { NavLink } from "react-router-dom";
+
+const Card = () => {
+  return (
+    <NavLink className="nav-link">
+      <div className="componente1">
+        
+      </div>
+    </NavLink>
+  );
+};
+
+export default Card;
